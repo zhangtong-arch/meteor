930659813f18799ae1c82a27b7f3040a368291e9
zhangtong-arch <1016128911@qq.com@example.com>
2019-07-28 13:15:09 +0800

    updata

---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <home></home>
+</template>
+
+<script>
+import home from "../components/home/home";
+export default {
+  components: {
+    home
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/pages/user/userManage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>用户管理</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
